406841ff6f05e90f664a0db9d634a5011e14538f
silexxx <daneshsgobbai@gmail.com>
2021-07-10 08:35:10 +0530

    5

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import sqlite3 as lite 
+import sys 
 
 
 username = st.sidebar.text_input('Username')
@@ -6,6 +8,32 @@ password = st.sidebar.text_input('Password')
 
 login_checkbox = st.sidebar.checkbox('Login')
 
+
+
+def deposit_db(name,email,city,zipcode,blood_group,blood_quantity,blood_bank_location,blood_bank_name):
+	# try:
+	# 	con = lite.connect('blood_bank.db') 
+	# 	cur = con.cursor()     
+	# 	cur.execute(f'''INSERT INTO depositions(NAME,EMAIL,CITY,ZIPCODE,BLOOD_GROUP,BLOOD_QUANTITY,BLOOD_BANK_LOCATION,BLOOD_BANK_NAME) VALUES (f'{name}', f'{email}', f'{city}', f'{zipcode}', f'{blood_group}',f'{blood_quantity}',f'{blood_bank_location}',f'{blood_bank_name}')''')
+	# 	con.commit()
+
+	# except Exception as e: 
+	# 	if con: 
+	# 		con.rollback() 
+
+	# 	print("Unexpected error %s:" % e.args[0]) 
+	# 	sys.exit(1) 
+	# finally: 
+	# 	if con: 
+	# 		con.close()
+
+	
+	con = lite.connect('blood_bank.db') 
+	cur = con.cursor()     
+	cur.execute(f'''INSERT INTO depositions(NAME,EMAIL,CITY,ZIPCODE,BLOOD_GROUP,BLOOD_QUANTITY,BLOOD_BANK_LOCATION,BLOOD_BANK_NAME) VALUES (f'{name}', f'{email}', f'{city}', f'{zipcode}', f'{blood_group}',f'{blood_quantity}',f'{blood_bank_location}',f'{blood_bank_name}')''')
+	con.commit()
+
+	
 
 
 
@@ -25,6 +53,9 @@ def individual():
 		submit_button = st.form_submit_button(label='Submit')
 
 	if submit_button:
+		global blood_bank_location,blood_bank_name
+		temp=deposit_db(name,email,city,zipcode,blood_group,blood_quantity,blood_bank_location,blood_bank_name)
+		print(temp)
 		st.write("records are saved")
 		return  name,email,city,zipcode,blood_group,blood_quantity
 
---
+++ test_db.py
@@ -0,0 +1,107 @@
+# creating table
+# import sqlite3 as lite 
+# import sys 
+ 
+# try: 
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute("CREATE TABLE depositions(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,CITY TEXT,ZIPCODE INTEGER,BLOOD_GROUP TEXT,BLOOD_QUANTITY INTEGER,BLOOD_BANK_LOCATION TEXT,BLOOD_BANK_NAME TEXT)")
+#     con.commit() 
+             
+# except e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
+
+
+# altering table
+# import sqlite3 as lite 
+# import sys 
+ 
+# try: 
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute("ALTER TABLE depositions(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,CITY TEXT,ZIPCODE INTEGER,BLOOD_GROUP TEXT,BLOOD_QUANTITY INTEGER,BLOOD_BANK_LOCATION TEXT,BLOOD_BANK_NAME TEXT)")
+#     con.commit() 
+             
+# except e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
+
+
+#drop tables
+# import sqlite3 as lite 
+# import sys 
+ 
+# try: 
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute("DROP TABLE depositions;")
+#     con.commit() 
+             
+# except e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
+
+
+#inserting row into database
+# import sqlite3 as lite 
+# import sys 
+ 
+# try: 
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute('''INSERT INTO depositions(NAME,EMAIL,CITY,ZIPCODE,BLOOD_GROUP,BLOOD_QUANTITY,BLOOD_BANK_LOCATION,BLOOD_BANK_NAME)
+# VALUES ('Daneshwar', 'daneshsgobbani@gmail.com', 'Belgaum', '590008', 'A+','100','HUBLI','KLE')''')
+#     con.commit() 
+             
+# except e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
+
+
+# import sqlite3 as lite 
+# import sys 
+ 
+# try:
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute('''select * from depositions''')
+#     con.commit()
+#     rows = cur.fetchall()
+
+#     for row in rows:
+#         print(row)
+             
+# except Exception as e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
