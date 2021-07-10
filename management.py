cbd271ba524444d06d7b92d995f9691184119486
silexxx <daneshsgobbai@gmail.com>
2021-07-10 17:32:34 +0530

    15

--- management.py
+++ management.py
@@ -15,61 +15,64 @@ def Add_New_Bank():
 		st.write('Please Enter the Details')
 		name =st.text_input('Please Enter Name of the New Bank')
 		location=st.text_input('Please Enter Location of the New Bank')
-		city =st.text_input('Please Enter city')
-		zipcode=st.text_input('Please Enter zipcode')
-		phone_no=st.number_input('Please Enter Phone Number')
-		phone_no=int(phone_no)
-		blood_group = st.selectbox('Please Choose Blood Group',('A+', 'A-', 'B+','B-','AB+','AB-','O+','O-'))
-		blood_quantity=st.number_input('Please Enter blood_quantity in ml')
-		blood_quantity=int(blood_quantity)
+		zipcode=st.text_input('Please Enter the zipcode of New Bank location')
 		submit_button = st.form_submit_button(label='Submit')
 
 	if submit_button:
-		global blood_bank_location,blood_bank_name
-		recordList=[(name,email,city,zipcode,phone_no,blood_group,blood_quantity,blood_bank_location,blood_bank_name)]
-		deposit_db(recordList)
 		st.write("Records are saved")
-		return  name,email,city,zipcode,phone_no,blood_group,blood_quantity,blood_bank_location,blood_bank_name
+		pass
+
+
+def Update_Excisting_Bank():
+	with st.form(key='Update_Excisting_Bank_form'):
+		blood_bank_location = st.selectbox('Please Choose Blood Bank location',('Belgaum','Hubli','BANGALORE'))
+		blood_bank_name_dict={'Belgaum':('KLE','JNMC'),'Hubli':('KIMS','LAKEVIEW'),'BANGALORE':('LAKEVIEW','APOLLO')}
+		print(blood_bank_name_dict[f'{blood_bank_location}'])
+		blood_bank_name = st.selectbox('Please Choose Blood Bank Name',blood_bank_name_dict[f'{blood_bank_location}'])
+		st.write('Please Enter the Details to Update')
+		name =st.text_input('Please Enter New Name of the  Bank')
+		location=st.text_input('Please Enter New Location of the Bank')
+		zipcode=st.text_input('Please Enter the New Bank location zipcode')
+		submit_button = st.form_submit_button(label='Submit')
+
+	if submit_button:
+		st.write("Records are saved")
+		pass
+
+def Assign_New_USER():
+	with st.form(key='Assign_New_USER_form'):
+		st.write('Please Enter the Details')
+		blood_bank_location = st.selectbox('Please Choose Blood Bank location',('Belgaum','Hubli','BANGALORE'))
+		blood_bank_name_dict={'Belgaum':('KLE','JNMC'),'Hubli':('KIMS','LAKEVIEW'),'BANGALORE':('LAKEVIEW','APOLLO')}
+		print(blood_bank_name_dict[f'{blood_bank_location}'])
+		blood_bank_name = st.selectbox('Please Choose Blood Bank Name',blood_bank_name_dict[f'{blood_bank_location}'])
+		name =st.text_input('Please Enter Name')
+		email=st.text_input('Please Enter Email')
+		city =st.text_input('Please Enter city')
+		zipcode=st.text_input('Please Enter zipcode')
+		phone_no=st.text_input('Please Enter Phone Number')
+		username=st.text_input('Please Enter usename to be assigned')
+		password=st.text_input('Please Enter password to be assigned')
+		submit_button = st.form_submit_button(label='Submit')
+
+	if submit_button:
+		st.write("Records are saved")
+		pass
+
+
 
 if login_checkbox:
 	if username=='1' and password=='1':
 		st.write('Logged In')
-
 		operation = st.sidebar.radio("Please Select type of operation:",('Add_New_Bank', 'Update_Existing_Bank','Assign_New_USER'))
+		if operation == 'Add_New_Bank':
+			Add_New_Bank()
+		elif operation == 'Update_Existing_Bank':
+			Update_Excisting_Bank()
 
-		
-
-		blood_bank_location = st.text_input('Please Enter New Bank Location')
-		blood_bank_name = t.text_input('Please Enter New Bank Name')
-
-
-		operation = st.sidebar.radio("Please Select type of operation:",('Deposit', 'Request_Processing'))
-		if operation == 'Deposit':
-
-			st.write('You selected Deposit.')
-			doners = st.sidebar.radio("Please Select An section",('Individual_Entry', 'Hospital_Entry','Donation_Camps_Entry'))
-
-			if doners == 'Individual_Entry':
-				st.write('You selected Individual_Entry.')
-				individual()
-			elif doners == 'Hospital_Entry':
-				st.write("You selected Hospital_Entry.")
-				hospitals()
-			elif doners == 'Donation_Camps_Entry':
-				st.write("You selected Donation_Camps_Entry.")
-				donation_camps()
+		elif operation == 'Assign_New_USER':
+			Assign_New_USER()
 			
-		elif operation == 'Request_Processing':
-			st.write("You selected Request_Processing.")
-
-
-
-
-		
-
-
-
-
 
 else:
 	st.write('Wrong Username or Password')
