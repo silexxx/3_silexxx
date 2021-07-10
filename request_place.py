3ebb85d629f3444e090788e0038c11fe65689fed
silexxx <daneshsgobbai@gmail.com>
2021-07-10 19:57:21 +0530

    18

--- request_place.py
+++ request_place.py
@@ -3,6 +3,27 @@ import sqlite3 as lite
 import sys
 import datetime
 
+st.title("Blood Request Portal")
+
+
+def requestplace_db(recordList):
+	try:
+		con = lite.connect('blood_bank.db') 
+		cur = con.cursor()     
+		sqlite_insert_query='''INSERT INTO BloodRequests(NAME ,EMAIL ,CITY ,ZIPCODE ,PHONENO ,BLOOD_GROUP ,BLOOD_TYPE , BLOOD_QUANTITY ,EXPIRY_DATE ,BLOOD_BANK_LOCATION ,BLOOD_BANK_NAME ,SHIP_ADDRESS) VALUES (?, ?, ?, ?, ?,?,?,?,?,?,?,?);'''
+		cur.executemany(sqlite_insert_query, recordList)
+		con.commit()
+
+	except Exception as e: 
+		if con: 
+			con.rollback() 
+
+		print("Unexpected error %s:" % e.args[0]) 
+		sys.exit(1) 
+	finally: 
+		if con: 
+			con.close()
+
 
 def blood_request():
 	with st.form(key='blood_request_form'):
@@ -12,22 +33,20 @@ def blood_request():
 		city =st.text_input('Please Enter city')
 		zipcode=st.text_input('Please Enter zipcode')
 		phone_no=st.text_input('Please Enter Phone Number')
-		# phone_no=int(phone_no)
 		blood_group = st.selectbox('Please Choose Blood Group',('A+', 'A-', 'B+','B-','AB+','AB-','O+','O-'))
 		type_of_blood=st.selectbox('Please Choose type ',('BLOOD_REPLACEMENT', 'PLASMA_REPLACEMENT'))
 		blood_quantity=st.number_input('Please Enter blood_quantity in ml')
 		blood_quantity=int(blood_quantity)
 		expiry_date=st.date_input("Till When do you want this request to be fulfilled",datetime.date(2020, 7, 10))
+		ship_address=st.text_input('Please Enter Ship Address')
 		submit_button = st.form_submit_button(label='Submit')
 
 	if submit_button:
+		global blood_bank_location,blood_bank_name
+		recordList=[(name,email,city,zipcode,phone_no,blood_group,type_of_blood,blood_quantity,expiry_date,blood_bank_location,blood_bank_name,ship_address)]
+		requestplace_db(recordList)
 		st.write("Request is submitted")
-		pass
-		# global blood_bank_location,blood_bank_name
-		# recordList=[(name,email,city,zipcode,phone_no,blood_group,blood_quantity,blood_bank_location,blood_bank_name)]
-		# deposit_db(recordList)
-		# st.write("Records are saved")
-		# return  name,email,city,zipcode,phone_no,blood_group,blood_quantity,blood_bank_location,blood_bank_name
+
 
 
 
--- request_place_db.py
+++ request_place_db.py
@@ -108,16 +108,60 @@
 
 
 #creating table requests
+# import sqlite3 as lite 
+# import sys 
+ 
+# try: 
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute("CREATE TABLE BloodRequests(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,CITY TEXT,ZIPCODE INTEGER,PHONENO INTEGER,BLOOD_GROUP TEXT,BLOOD_TYPE TEXT, BLOOD_QUANTITY INTEGER,EXPIRY_DATE TEXT,BLOOD_BANK_LOCATION TEXT,BLOOD_BANK_NAME TEXT,SHIP_ADDRESS TEXT)")
+#     con.commit() 
+             
+# except Exception as e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
+
+#drop table
+# import sqlite3 as lite 
+# import sys 
+ 
+# try: 
+#     con = lite.connect('blood_bank.db') 
+#     cur = con.cursor()     
+#     cur.execute("DROP TABLE BloodRequests;")
+#     con.commit() 
+             
+# except e: 
+#     if con: 
+#         con.rollback() 
+     
+#     print("Unexpected error %s:" % e.args[0]) 
+#     sys.exit(1) 
+# finally: 
+#     if con: 
+#         con.close()
+
+# check values
 import sqlite3 as lite 
 import sys 
  
-try: 
+try:
     con = lite.connect('blood_bank.db') 
     cur = con.cursor()     
-    cur.execute("CREATE TABLE BloodRequests(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,CITY TEXT,ZIPCODE INTEGER,PHONENO INTEGER,BLOOD_GROUP TEXT,BLOOD_TYPE TEXT, BLOOD_QUANTITY INTEGER,EXPIRY_DATE TEXT,BLOOD_BANK_LOCATION TEXT,BLOOD_BANK_NAME TEXT,SHIP_ADDRESS TEXT)")
-    con.commit() 
+    cur.execute('''select * from BloodRequests''')
+    con.commit()
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
              
-except e: 
+except Exception as e: 
     if con: 
         con.rollback() 
      
@@ -126,6 +170,3 @@ except e:
 finally: 
     if con: 
         con.close()
-
-
-
